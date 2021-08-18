28339c178197bc092ae578eb6feee8808fed80a5
Marcin <marcinkufel84@gmail.com>
2021-08-18 07:42:32 +0200

    added post request

--- cypress/integration/request.js
+++ cypress/integration/request.js
@@ -12,4 +12,28 @@ describe('Request command suite', () => {
         })
     })
 
+    it('Get request', () => {
+        cy.visit('http://localhost:3000')
+        cy.request({
+            method: 'GET',
+            url:'http://localhost:8080/todos',
+            qs: {
+                "id": 1
+            },
+
+        }).then(response => {
+
+        expect(response.body[0].isComplete).to.be.true
+        })
+    })
+
+    it.only('post request', () => {
+        cy.request('POST', 'http://localhost:8080/todos', {
+            "name": "test1",
+            "isComplete": false
+        }).then(response => {
+            expect(response.status).to.eq(201)
+    })
+    })
+
 })
