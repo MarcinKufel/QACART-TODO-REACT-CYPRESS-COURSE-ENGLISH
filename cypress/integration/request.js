b40f5a690df5226249f07f354e12b81eea532fc0
Marcin <marcinkufel84@gmail.com>
2021-08-18 07:07:53 +0200

    added request and response

--- cypress/integration/request.js
+++ cypress/integration/request.js
@@ -4,7 +4,10 @@
 describe('Request command suite', () => {
 
     it('Get request', () => {
-        
+        cy.request('GET', 'http://localhost:8080/todos').then(response => {
+
+        cy.log(response.status)
+        })
     })
 
 })
