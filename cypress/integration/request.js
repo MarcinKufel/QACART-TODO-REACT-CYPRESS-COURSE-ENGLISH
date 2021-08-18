5bd485c30bb6005f5fd43e91d70b4c18e7c8e1b2
Marcin <marcinkufel84@gmail.com>
2021-08-18 13:59:08 +0200

    added tests to secured api

--- cypress/integration/request.js
+++ cypress/integration/request.js
@@ -41,7 +41,27 @@ describe('Request command suite', () => {
         cy.request('PUT', 'http://localhost:8080/todos/7147', {"name":"test1","isComplete":true,"id":7147})
     })
 
-    it.only('Delete', () => {
+    it('Delete', () => {
         cy.request('DELETE', 'http://localhost:8080/todos/7150')
+    })
+
+    it('secured api', () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:8080/courses',
+            auth: {
+                bearer: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9saXZpZXIyMjJAbWFpbC5jb20iLCJpYXQiOjE2MjkyODcwMTAsImV4cCI6MTYyOTI5MDYxMCwic3ViIjoiNSJ9.7VqIjzRszQvpna8mgG14YRF0dm19nyN-2t7-Z9lw97c'
+            }
+        })
+    })
+
+    it.only('secured api2', () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:8080/courses',
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9saXZpZXIyMjJAbWFpbC5jb20iLCJpYXQiOjE2MjkyODcwMTAsImV4cCI6MTYyOTI5MDYxMCwic3ViIjoiNSJ9.7VqIjzRszQvpna8mgG14YRF0dm19nyN-2t7-Z9lw97c"
+            }
+        })
     })
 })
