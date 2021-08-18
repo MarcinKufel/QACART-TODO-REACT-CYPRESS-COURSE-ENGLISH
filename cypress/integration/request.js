c826f318db70a093254c976d683ef508d8278811
Marcin <marcinkufel84@gmail.com>
2021-08-18 07:14:10 +0200

    added assertions to test

--- cypress/integration/request.js
+++ cypress/integration/request.js
@@ -1,4 +1,4 @@
-/// <reference types="Cypress" />
+///<reference types="Cypress"/>
 
 
 describe('Request command suite', () => {
@@ -6,7 +6,9 @@ describe('Request command suite', () => {
     it('Get request', () => {
         cy.request('GET', 'http://localhost:8080/todos').then(response => {
 
-        cy.log(response.status)
+        expect(response.status).to.be.eq(200)
+        expect(response.duration).to.be.below(20000)
+        expect(response.body[0].isComplete).to.be.true
         })
     })
 
