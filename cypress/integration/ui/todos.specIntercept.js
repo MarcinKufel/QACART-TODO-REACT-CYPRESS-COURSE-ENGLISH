22fe834ff98df7b1d73e5bef4544f3a647c6d99a
Marcin <marcinkufel84@gmail.com>
2021-08-19 12:46:10 +0200

    added intercept command do test

---
+++ cypress/integration/ui/todos.specIntercept.js
@@ -0,0 +1,47 @@
+///<reference types="Cypress"/>
+
+
+describe('Todo UI testing', () => {
+
+    beforeEach(() => {
+        cy.visit("/")
+    })
+
+    it.only('should add a new todo correctly', () => {
+ //       cy.get('.todo-input').type('First Todo').type('{enter}')
+          cy.intercept('POST', 'http://localhost:8080/todos').as('postRequest')  
+          cy.addNewTodo('First Todo');  
+          cy.wait('@postRequest').then(xhr => {
+              expect(xhr.request.body.name).to.eql('First Todo')
+          })
+          cy.get('.todo-item').last().should('contain.text', 'First Todo')
+
+    })
+
+    it ('should be able to toggle the status of a todo correctly', () => {
+        cy.addNewTodo("Second Todo");
+        cy.get('.todo-checkbox').check().should('be.checked');
+        cy.get('.todo-checkbox').uncheck().should('not.be.checked');
+
+    })
+
+    it('should delete a todo correctly', () => {
+        cy.addNewTodo("Third Todo");
+        cy.get('.delete-item').click({multiple: true});
+
+    })
+
+    it('should not add an empty todo', ()=>{
+        cy.addNewTodo("");
+
+    })
+
+    afterEach(() => {
+        cy.get('body').then($el => {
+            if($el.find('.delete-item').length > 0) {
+                cy.get('.delete-item').click({multiple: true});
+            }
+        })
+        
+    })
+})
