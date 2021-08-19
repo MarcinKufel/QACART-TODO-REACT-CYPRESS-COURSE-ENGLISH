34cffb1a54f0641f8654c11e3b37f030f2afe210
Marcin <marcinkufel84@gmail.com>
2021-08-19 07:02:23 +0200

    added script to test case

--- cypress/integration/ui/todos.spec.js
+++ cypress/integration/ui/todos.spec.js
@@ -9,15 +9,13 @@ describe('Todo UI testing', () => {
 
     it('should add a new todo correctly', () => {
  //       cy.get('.todo-input').type('First Todo').type('{enter}')
-          cy.get('.todo-input').type('First Todo{enter}');
-          cy.get('.success').should('be.visible');
+          cy.addNewTodo('First Todo');  
           cy.get('.todo-item').last().should('contain.text', 'First Todo')
 
     })
 
     it ('should be able to toggle the status of a todo correctly', () => {
-        cy.get('.todo-input').type('First Todo{enter}');
-        cy.get('.success').should('be.visible');
+        cy.addNewTodo("Second Todo");
         cy.get('.todo-checkbox').check().should('be.checked');
         cy.get('.todo-checkbox').uncheck().should('not.be.checked');
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("addNewTodo", (todo) => {
+    cy.get('.todo-input').type(todo + '{enter}');
+    cy.get('.success').should('be.visible');
+})
